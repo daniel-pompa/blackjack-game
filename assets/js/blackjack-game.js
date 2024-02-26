e8afeba0c9b865ec0996823ec313d5ec30faa0cf
Daniel <daniel.40.pompa@gmail.com>
2024-02-26 23:32:19 +0100

    feat: deal the initial cards
    function to deal the player's and dealer's initial cards

--- assets/js/blackjack-game.js
+++ assets/js/blackjack-game.js
@@ -2,12 +2,16 @@
 let deck = []; // Initialize an empty array to hold the deck of cards
 const suits = ['C', 'D', 'H', 'S']; // Array of card suits
 const specials = ['A', 'J', 'Q', 'K']; // Array of special card values
+let playerHand = []; // Initialize an empty array to hold player's hand
+let dealerHand = []; // Initialize an empty array to hold dealer's hand
 
 //* Functions
-// Function to start the game
+// Start the game by creating a deck of cards and dealing initial cards to player and dealer
 const startGame = () => {
   // Call the function to create a deck of cards
   createDeck();
+  // Call the function to deal initial cards to player and dealer
+  dealInitialCards();
 };
 
 /**
@@ -27,7 +31,6 @@ const createDeck = () => {
       deck.push(special + suit); // Add special card and suit to deck
     }
   }
-  console.log(deck);
   // Call the shuffleDeck function and return the shuffled deck
   return shuffleDeck(deck);
 };
@@ -42,12 +45,25 @@ const shuffleDeck = deck => {
   for (let i = deck.length - 1; i > 0; i--) {
     // Generate a random index 'j' within the range of 0 to i
     const j = Math.floor(Math.random() * (i + 1));
-    // Swap the elements at indices i and j in the deck
+    // Swap the elements at indices 'i' and 'j' in the deck
     [deck[i], deck[j]] = [deck[j], deck[i]];
   }
-  // Output the shuffled deck to the console
-  console.log(deck);
   return deck; // Return the shuffled deck
+};
+
+// Function to deal initial cards to the player and the dealer
+const dealInitialCards = () => {
+  // Deal 2 cards to the player and the dealer
+  for (let i = 0; i < 2; i++) {
+    playerHand.push(deck.pop());
+    dealerHand.push(deck.pop());
+  }
+  // TODO - Add code to display the cards on the UI
+  // Display the player's hand in the console
+  console.log(`Mano del jugador: ${playerHand}`);
+  // Display the dealer's hand in the console
+  console.log(`Mano del crupier: ${dealerHand}`);
+  // TODO - Add code to calculate the total value of the player's adn dealer's hand
 };
 
 startGame();
